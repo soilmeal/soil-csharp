6d349d9a07c4032b0115ed1e3f610e2c36a02ad4
KIM YONG JE <rlatns3@gmail.com>
2021-11-08 22:25:33 +0900

    [dev/core/threading/tasks] TaskScheduler 수정
    1. FixedThreadTaskScheduler 삭제
    2. SingleThreadTaskScheduler 삭제
    3. 삭제한 TaskScheduler 들을 한 번에 커버할 수 있도록 TaskScheduler 작성

--- src/Soil.Core/Threading/Tasks/TaskScheduler.cs
+++ src/Soil.Core/Threading/Tasks/TaskScheduler.cs
@@ -1,13 +1,68 @@
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace Soil.Core.Threading.Tasks
 {
-    public abstract class TaskScheduler : System.Threading.Tasks.TaskScheduler
+    public class TaskScheduler : System.Threading.Tasks.TaskScheduler
     {
-        private readonly IThreadFactory _threadFactory;
-        public IThreadFactory ThreadFactory { get => _threadFactory; }
+        private readonly int _maximumConcurrencyLevel;
+        public override int MaximumConcurrencyLevel => _maximumConcurrencyLevel;
 
-        public TaskScheduler(IThreadFactory threadFactory)
+        private readonly IThreadFactory _threadFactory;
+        public IThreadFactory ThreadFactory => _threadFactory;
+
+        private readonly BlockingCollection<Task> _tasks;
+
+        private readonly List<Thread> _threads;
+
+        public TaskScheduler(int maximumConcurrencyLevel_, IThreadFactory threadFactory_) :
+            this(maximumConcurrencyLevel_, threadFactory_, new())
+        { }
+
+        public TaskScheduler(int maximumConcurrencyLevel_, IThreadFactory threadFactory_, BlockingCollection<Task> tasks_)
         {
-            _threadFactory = threadFactory;
+            // call and discard to create unique id of scheduler
+            _ = Id;
+
+            _maximumConcurrencyLevel = maximumConcurrencyLevel_;
+            _threadFactory = threadFactory_;
+            _tasks = tasks_;
+
+            _threads = Enumerable.Range(0, maximumConcurrencyLevel_)
+                .Select(i => ThreadFactory.Create(Execute))
+                .ToList();
+            _threads.ForEach(thread => thread.Start());
+        }
+
+        protected override IEnumerable<Task>? GetScheduledTasks()
+        {
+            return _tasks.ToArray();
+        }
+
+        protected override void QueueTask(Task task_)
+        {
+            _tasks.Add(task_);
+        }
+
+        protected override bool TryExecuteTaskInline(Task task_, bool taskWasPreviouslyQueued_)
+        {
+            return false;
+        }
+
+        protected override bool TryDequeue(Task task_)
+        {
+            return false;
+        }
+
+        private void Execute()
+        {
+            foreach (var task in _tasks.GetConsumingEnumerable())
+            {
+                TryExecuteTask(task);
+            }
         }
     }
 }
