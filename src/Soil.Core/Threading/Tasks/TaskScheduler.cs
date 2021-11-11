b2f25e83d5a5360fc05c3e4fab72b4de753f529e
KIM YONG JE <rlatns3@gmail.com>
2021-11-11 23:05:17 +0900

    [dev/core/threading/tasks] TaskScheduler 수정
    1.  ThreadFactory 의 제어자 virtual -> abstract 로 수정

--- src/Soil.Core/Threading/Tasks/TaskScheduler.cs
+++ src/Soil.Core/Threading/Tasks/TaskScheduler.cs
@@ -2,5 +2,5 @@ namespace Soil.Core.Threading.Tasks;
 
 public abstract class TaskScheduler : System.Threading.Tasks.TaskScheduler
 {
-    public virtual IThreadFactory ThreadFactory => FormattedNameThreadFactory.Default;
+    public abstract IThreadFactory ThreadFactory { get; }
 }
