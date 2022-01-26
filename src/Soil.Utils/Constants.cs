43d3a63e1805acdd8b8423dee5bc79b1b3d0b7f3
KIM YONG JE <rlatns3@gmail.com>
2022-01-26 18:27:03 +0900

    [dev/util] Soil.Util 프로젝트 생성 및 작성
    1. 콘솔 앱에서 DOTNET_ENVIRONMENT 에 설정되어 있는 값을 가져오는 EnvrionmentUtil 클래스 추가

---
+++ src/Soil.Utils/Constants.cs
@@ -0,0 +1,12 @@
+namespace Soil.Utils;
+
+public static class Constants
+{
+    public const string DotnetEnvironmentNameKey = "DOTNET_ENVIRONMENT";
+
+    public const string DotnetEnvrionmentNameDevelopment = "Development";
+
+    public const string DotnetEnvironmentNameProduction = "Production";
+
+    public const string DefaultDotnetEnvrionmentName = DotnetEnvrionmentNameDevelopment;
+}
---
+++ src/Soil.Utils/EnvironmentUtil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Soil.Utils;
+
+public static class EnvironmentUtil
+{
+    private static readonly string _dotnetEnvironmentName = Environment.GetEnvironmentVariable(Constants.DotnetEnvironmentNameKey) ?? Constants.DefaultDotnetEnvrionmentName;
+
+    public static string DotnetEnvironmentName
+    {
+        get
+        {
+            return _dotnetEnvironmentName;
+        }
+    }
+
+    public static bool IsDevelopment()
+    {
+        return string.Equals(_dotnetEnvironmentName, Constants.DotnetEnvrionmentNameDevelopment);
+    }
+
+    public static bool IsProduction()
+    {
+        return string.Equals(_dotnetEnvironmentName, Constants.DotnetEnvironmentNameProduction);
+    }
+}
