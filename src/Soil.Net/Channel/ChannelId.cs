7a60b68e711dcbde11cd0b6a385de6036ca613c5
KIM YONG JE <rlatns3@gmail.com>
2022-02-18 02:35:00 +0900

    [dev/net] Net 프로젝트 작업
    1. ChannelId 가 변경된 HashCodeBasedId 구현을 따르도록 수정
    2. IEquatable<T> 를 구현하도록 수정
    3. equality, inequality 연산자 재정의 작업

--- src/Soil.Net/Channel/ChannelId.cs
+++ src/Soil.Net/Channel/ChannelId.cs
@@ -1,11 +1,69 @@
+using System;
 using Soil.Utils.Id;
 
 namespace Soil.Net.Channel;
 
-public class ChannelId : HashCodeBasedId
+#pragma warning disable CS0659, CS0661
+public class ChannelId : HashCodeBasedId, IEquatable<ChannelId>
 {
     public ChannelId(byte[] bytes)
         : base(bytes)
     {
     }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is ChannelId id && Equals(id);
+    }
+
+    public override bool Equals(IInt64Id? other)
+    {
+        return other is ChannelId id && Equals(id);
+    }
+
+    public override bool Equals(long other)
+    {
+        return Value == other;
+    }
+
+    public bool Equals(ChannelId? other)
+    {
+        return Equals(this, other);
+    }
+
+    public static bool operator ==(ChannelId? channelId, long other)
+    {
+        return channelId != null && channelId.Equals(other);
+    }
+
+    public static bool operator !=(ChannelId? channelId, long other)
+    {
+        return channelId != null && !channelId.Equals(other);
+    }
+
+    public static bool operator ==(ChannelId? lhs, ChannelId? rhs)
+    {
+        return Equals(lhs, rhs);
+    }
+
+    public static bool operator !=(ChannelId? lhs, ChannelId? rhs)
+    {
+        return !Equals(lhs, rhs);
+    }
+
+    private static bool Equals(ChannelId? lhs, ChannelId? rhs)
+    {
+        if (ReferenceEquals(lhs, rhs))
+        {
+            return true;
+        }
+
+        if (lhs is null || rhs is null)
+        {
+            return false;
+        }
+
+        return lhs.Value == rhs.Value;
+    }
 }
+#pragma warning restore CS0659, CS0661
